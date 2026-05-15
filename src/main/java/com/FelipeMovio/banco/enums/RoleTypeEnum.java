57eddd515c68440fb8aa179513dc4c3f78811697
FelipeMovio <felipelsmovio@gmail.com>
2026-05-15 16:01:42 -0300

    update

---
+++ src/main/java/com/FelipeMovio/banco/enums/RoleTypeEnum.java
@@ -0,0 +1,7 @@
+package com.FelipeMovio.banco.enums;
+
+public enum RoleTypeEnum {
+    ROLE_ADMIN,
+    ROLE_CORRENTE,
+    ROLE_POUPANCA
+}
---
+++ src/main/java/com/FelipeMovio/banco/model/UsuarioEntity.java
@@ -0,0 +1,73 @@
+package com.FelipeMovio.banco.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UsuarioEntity implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    private String senha;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(joinColumns = @JoinColumn(name = "role_id"))
+    private Set<RoleEntity> role = new HashSet<>();
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return role;
+    }
+
+    @Override
+    public @Nullable String getPassword() {
+        return senha;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
