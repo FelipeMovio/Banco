b4489741eed55d71d13a1b2db5657411f8ee16b2
FelipeMovio <felipelsmovio@gmail.com>
2026-05-25 12:35:30 -0300

    transacoes Entity create

---
+++ src/main/java/com/FelipeMovio/banco/database/model/TransacoesEntity.java
@@ -0,0 +1,38 @@
+package com.FelipeMovio.banco.database.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@Builder
+public class TransacoesEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Double valor;
+
+    @JoinColumn(name = "recebedor_id")
+    @ManyToOne
+    private UsuarioEntity recebedor;
+
+    @JoinColumn(name = "pagador_id")
+    @ManyToOne
+    private UsuarioEntity pagador;
+
+    private LocalDateTime dataHoraTransacao;
+
+    @PrePersist
+    void prePersist() {
+        this.dataHoraTransacao = LocalDateTime.now();
+    }
+}
--- src/main/java/com/FelipeMovio/banco/service/TransferenciaService.java
+++ src/main/java/com/FelipeMovio/banco/service/TransferenciaService.java
@@ -1,9 +1,16 @@
 package com.FelipeMovio.banco.service;
 
+import com.FelipeMovio.banco.database.repository.ContaRepository;
+import com.FelipeMovio.banco.database.repository.DadosRepositoy;
+import com.FelipeMovio.banco.database.repository.UsuarioRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class TransferenciaService {
+
+    private final UsuarioRepository usuarioRepository;
+    private final DadosRepositoy dadosRepositoy;
+    private final ContaRepository contaRepository;
 }
