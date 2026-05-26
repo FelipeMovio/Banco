a5570c01d674d7cff509c255a3082767be01c227
FelipeMovio <felipelsmovio@gmail.com>
2026-05-26 12:28:07 -0300

    update

--- src/main/java/com/FelipeMovio/banco/service/TransferenciaService.java
+++ src/main/java/com/FelipeMovio/banco/service/TransferenciaService.java
@@ -1,8 +1,11 @@
 package com.FelipeMovio.banco.service;
 
+import com.FelipeMovio.banco.database.model.TransacoesEntity;
 import com.FelipeMovio.banco.database.repository.ContaRepository;
 import com.FelipeMovio.banco.database.repository.DadosRepositoy;
 import com.FelipeMovio.banco.database.repository.UsuarioRepository;
+import com.FelipeMovio.banco.dto.TransacaoDto;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -13,4 +16,11 @@ public class TransferenciaService {
     private final UsuarioRepository usuarioRepository;
     private final DadosRepositoy dadosRepositoy;
     private final ContaRepository contaRepository;
+
+    @Transactional
+    public TransacoesEntity transferirValores(TransacaoDto transacaoDtod) {
+
+    }
+
+
 }
