c5def22f9085d4eb5d7b0e827d10546cbb04357d
FelipeMovio <felipelsmovio@gmail.com>
2026-06-30 20:34:28 -0300

    create repository by pixKey

---
+++ src/main/java/com/FelipeMovio/banco/database/repository/PixKeyRepository.java
@@ -0,0 +1,16 @@
+package com.FelipeMovio.banco.database.repository;
+
+import com.FelipeMovio.banco.database.model.PixKeyEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface PixKeyRepository extends JpaRepository<PixKeyEntity, Long> {
+
+    Optional<PixKeyEntity> findByChave(String chave);
+
+    boolean existsByChave(String chave);
+
+    List<PixKeyEntity> findAllByContaId(Long contaId);
+}
